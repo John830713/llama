3ba6be1bfb87ba03c8fe707094fe83d730e1a3e2
John_SW_Wang <john_sw_wang@wistron.com>
2026-07-14 19:46:25 +0800

    Add start-server.bat: launch server + list models

---
+++ start-server.bat
@@ -0,0 +1,18 @@
+@echo off
+chcp 65001 >nul
+
+set "HOST=127.0.0.1"
+set "PORT=9090"
+
+echo Starting llama-server...
+start "llama_server" D:\llama\bin\llama-server.exe --models-dir "D:\Models" --host %HOST% --port %PORT% -ngl 10 -c 4096
+
+echo Waiting for server...
+timeout /t 3 /nobreak >nul
+
+echo.
+echo === Model List ===
+curl.exe -s http://%HOST%:%PORT%/v1/models
+echo.
+echo.
+pause
